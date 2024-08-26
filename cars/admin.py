c488b5032284f85dc258dfcaf696e4aa403d052c
matheusmneves <matheusneves.m@gmail.com>
2024-08-26 16:11:05 -0600

    Model adjust

--- cars/admin.py
+++ cars/admin.py
@@ -4,7 +4,7 @@ from cars.models    import Brand
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'licence_plate', 'value')
-    search_fields = ('model',)
+    search_fields = ('model', 'brand',)
 
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name',)
