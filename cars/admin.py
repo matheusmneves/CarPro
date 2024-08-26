b621aeb70e23e6f9c08da6a4fe909e8eacd7150b
Matheus Neves <matheusneves@Mac.lan>
2024-08-26 16:00:06 -0600

    New models

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from cars.models    import Car
+from cars.models    import Brand
 
-# Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'licence_plate', 'value')
+    search_fields = ('model',)
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    
+admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    licence_plate = models.CharField(max_length=10, blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+
+    def __str__(self):
+        return self.model
