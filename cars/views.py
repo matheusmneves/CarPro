96cea1f900ce9b91ea65181ca216ec4d486e0ecc
matheusmneves <matheusneves.m@gmail.com>
2024-08-29 20:04:41 -0600

    Views update

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,11 @@ from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('factory_year')
+    search = request.GET.get('search')
+    
+    if search:
+        cars = cars.filter(model__icontains=search)
     
     return render(
         request,
