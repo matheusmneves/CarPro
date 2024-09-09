06cf828145f0daa4b947e0b4960e69e5709e5778
matheusmneves <matheusneves.m@gmail.com>
2024-09-09 15:23:35 -0600

    Register view update

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register_view(request):
-    user_form = UserCreationForm()
+    if request.method == "POST":
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('cars_list')
+    else:
+        user_form = UserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
